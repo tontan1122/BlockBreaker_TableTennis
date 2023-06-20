df2e49dcb8994d4b89236f38d9ddb47c27583aca
TOMOYAPC\wanbo <wanbon.t.n.1122@gmail.com>
2023-06-20 22:38:35 +0900

    プレイヤーの行動範囲の制限、カーソルに関する処理、ボールの状態遷移の作成

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField, Header("移動スピード")]
+    private float moveSpeed = 10.0f;
+
+    [SerializeField, Header("移動範囲:X")]
+    private float moveLimitX;
+
     private new Rigidbody2D rigidbody;
+
 
     void Start()
     {
@@ -14,11 +19,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector2 mousePosition = Input.mousePosition;
-        // カーソル位置をワールド座標に変換
-        Vector2 target = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector2 target = Camera.main.ScreenToWorldPoint(mousePosition);  // カーソル位置をワールド座標に変換
         Vector2 direction = target - (Vector2)transform.position;
         direction.y = 0;
 
-        rigidbody.velocity = direction * 10;
+        rigidbody.velocity = direction * moveSpeed;
+
+        /*画面端処理*/
+        Vector3 currentPos = transform.position;
+
+        //Mathf.ClampでXの値を最小～最大の範囲内に収める。
+        currentPos.x = Mathf.Clamp(currentPos.x, -moveLimitX, moveLimitX);
+
+        transform.position = currentPos;
     }
+
 }
